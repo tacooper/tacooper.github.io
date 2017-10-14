fcf2a062489561cb50dd4524046429bbf26f6201
Tommy Cooper <tacooper@wrapperapps.com>
2017-10-14 16:01:14 -0400

    Added main JS class that initializes 2-D context from canvas.

---
+++ js/headgames.js
@@ -0,0 +1,7 @@
+// initialize main class
+var Headgames = function() {
+    this.canvas = document.getElementById("myCanvas");
+    this.context = this.canvas.getContext("2d");
+}
+
+var headgames = new Headgames();

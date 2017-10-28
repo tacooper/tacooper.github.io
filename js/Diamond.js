b8c3690f8b6cea862addcd7ab8c583217a518c9c
Tom Cooper <tacooper@vt.edu>
2017-10-28 13:44:38 -0400

    Replaced update/draw interval with requestAnimationFrame loop.
    -Generated diamond sprites in separate interval loop.
    -Restarted game after resizing window.
    -Filled entire window with canvas.

--- js/Diamond.js
+++ js/Diamond.js
@@ -3,8 +3,8 @@ var Diamond = function(context, canvas, image) {
     this.context = context;
     this.image = image;
     
-    this.x = canvas.width / 2
-    this.y = canvas.height / 2;
+    this.x = (canvas.width / 2) - (this.image.width / 2);
+    this.y = (-this.image.height);
 }
 
 //draw state every frame
@@ -14,6 +14,5 @@ Diamond.prototype.draw = function() {
 
 //update state every frame
 Diamond.prototype.update = function() {
-    this.x += 1;
-    this.y += -1;
+    this.y += 1;
 }
--- js/Main.js
+++ js/Main.js
@@ -3,56 +3,71 @@ const IMAGE_URLS = {
     diamond: "img/Diamond.png"
 };
 
-// initialize class
-var Main = function() {
-    var canvas = document.getElementById("myCanvas");
-    var context = canvas.getContext("2d");
+var generateInterval;
+var canvas;
+var context;
+var sprites = [];
+
+var main = function() {
+    // reset any existing intervals (in case of resize)
+    clearInterval(generateInterval);
+    
+    // configure canvas size
+    canvas = document.getElementById("mainCanvas");
+    context = canvas.getContext("2d");
+    canvas.width = document.documentElement.clientWidth;
+	canvas.height = document.documentElement.clientHeight;
     
     // load images into map
-    var imageMap = this.loadImages(IMAGE_URLS);
+    var imageMap = loadImages(IMAGE_URLS);
     
-    // handle sprites each frame (every 10 ms)
-    var sprites = [];
-    setInterval(function() {
+    // generate sprites periodically (every 1 sec)
+    generateInterval = setInterval(function() {
         // check if all images are loaded
-        if (sprites.length == 0 &&
-            Object.keys(imageMap).length == Object.keys(IMAGE_URLS).length) {
+        if (Object.keys(imageMap).length == Object.keys(IMAGE_URLS).length) {
             // create sprites from images
             var diamond = new Diamond(context, canvas, imageMap["diamond"]);
             sprites.push(diamond);
         }
-        
-        // check if any sprites exist
-        if (sprites.length > 0) {
-            // clear canvas every frame
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            
-            // draw sprites every frame
-            sprites.forEach(function(sprite) {
-                sprite.draw();
-            });
-            
-            // update state of sprites every frame
-            sprites.forEach(function(sprite) {
-                sprite.update();
-            });
-        }
-    }, 10);
+    }, 1000);
+    
+    // start loop for updating/drawing each frame
+    loop();
 }
 
 // load each image from URL into map
-Main.prototype.loadImages = function(urls) {
+var loadImages = function(urls) {
     var imageMap = {};
     Object.keys(urls).forEach(function(id) {
         var url = IMAGE_URLS[id];
         var img = new Image();
         img.onload = function() {
+            // store each image in map after loading
             imageMap[id] = img;
-            console.log(imageMap.length); //TODO
+            console.log("images loaded: " + Object.keys(imageMap).length); //TODO
         };
         img.src = url;
     });
     return imageMap;
 }
 
-var main = new Main();
+var loop = function() {
+    // handle existing sprites each frame
+    if (sprites.length > 0) {
+        // clear canvas each frame
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        
+        // update state of sprites each frame
+        sprites.forEach(function(sprite) {
+            sprite.update();
+        });
+        
+        // draw sprites each frame
+        sprites.forEach(function(sprite) {
+            sprite.draw();
+        });
+    }
+    
+    // continue loop on next frame
+    requestAnimationFrame(loop);
+}

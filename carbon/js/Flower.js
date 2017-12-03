6c1f45ed149159c58e469b9f5ff3390b533e4870
Tom Cooper <tacooper@vt.edu>
2017-12-03 17:26:25 -0500

    Animated flower (incomplete) by updating/drawing subimage.
    -Updated flower image to include first 8 subimages in
      spritesheet format.

--- carbon/js/Flower.js
+++ carbon/js/Flower.js
@@ -5,16 +5,26 @@ var Flower = function(canvas, flowerpot) {
     // set initial state at bottom of canvas
     this.enoughDirt = flowerpot.enoughDirt;
     this.flowerpotHeight = flowerpot.img.height;
-    this.x = (canvas.width - this.img.width) / 2;
+    this.SUBIMAGE_WIDTH = 240;
+    this.x = (canvas.width - this.SUBIMAGE_WIDTH) / 2;
     this.y = canvas.height - this.flowerpotHeight - (this.img.height / 2);
     this.Y_RATE = -1; //pixels per frame
+    this.NUM_SUBIMAGES = 8;
+    this.subimageIndex = 0;
+    this.FRAMES_PER_SUBIMAGE = 10;
+    this.frameCount = 0;
 }
 Flower.prototype = Object.create(Sprite.prototype);
 Flower.prototype.constructor = Flower;
 
 // draw image every frame
 Flower.prototype.draw = function(context) {
-    context.drawImage(this.img, this.x, this.y);
+    // only draw subimage at specified index
+    const sourceX = (this.subimageIndex * this.SUBIMAGE_WIDTH);
+    
+    context.drawImage(this.img,
+        sourceX, 0, this.SUBIMAGE_WIDTH, this.img.height,
+        this.x, this.y, this.SUBIMAGE_WIDTH, this.img.height);
 }
 
 // update state every frame
@@ -24,7 +34,17 @@ Flower.prototype.update = function(canvas) {
     // only translate until entire sprite is displayed on canvas
     if (this.y > finalY) {
         this.y += this.Y_RATE;
+        
+    // only animate subimages if flowerpot has enough dirt
+    // (or just show first subimage of sprout)
     } else if (this.enoughDirt) {
-        //TODO: animate flower from sprite sheet
+        ++this.frameCount;
+        
+        // update index for subimage at partial frame rate
+        if (this.subimageIndex < (this.NUM_SUBIMAGES - 1) &&
+            this.frameCount >= this.FRAMES_PER_SUBIMAGE) {
+            ++this.subimageIndex;
+            this.frameCount = 0;
+        }
     }
 }

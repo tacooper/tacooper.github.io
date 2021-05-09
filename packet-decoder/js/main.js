e287ee2f6b6a1353e9b54dd28e6847847ae89413
Tom Cooper <tacooper@vt.edu>
2021-05-09 17:56:14 -0400

    Added input for specifying raw packet.
    * Added button for decoding raw packet (TODO).

--- packet-decoder/js/main.js
+++ packet-decoder/js/main.js
@@ -3,4 +3,19 @@ $(function () {
     // display version number
     var $versionSpan = $("#version-span");
     $versionSpan.text("Version: " + VERSION_NUMBER);
+
+    // configure callback for clicking decode button
+    var $decodeButton = $("#decode-button");
+    $decodeButton.click(function() {
+        // decode raw packet into schema sub-fields
+        decodeRawPacket();
+    });
 });
+
+var decodeRawPacket = function() {
+    // get raw packet from input
+    var $rawPacketInput = $("#raw-packet-input");
+    var rawPacket = $rawPacketInput.val();
+
+    // TODO: handle decoding raw packet into schema sub-fields
+}

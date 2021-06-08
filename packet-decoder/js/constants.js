79cd5e90c2b87ec0930a1a8fbae59dbccbc9d02c
Tom Cooper <tacooper@vt.edu>
2021-06-07 22:38:05 -0400

    Updated Packet Decoder version number to 1.4.0.

--- packet-decoder/js/constants.js
+++ packet-decoder/js/constants.js
@@ -1,5 +1,5 @@
 // Packet Decoder version number
-const VERSION_NUMBER = "1.3.1";
+const VERSION_NUMBER = "1.4.0";
 
 // characters for separating each sub-field in decoded packet
 const SUBFIELD_START_SEPARATOR = "[ ";
